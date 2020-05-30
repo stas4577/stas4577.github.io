145435638dbe422377c71f872100b694a8e30ac7
stas4577 <54317704+stas4577@users.noreply.github.com>
2020-05-30 16:10:37 +0300

    news

---
+++ news/scripts/script.js
@@ -0,0 +1,37 @@
+
+
+
+$(document).ready(function () {
+	function searchOpen () {
+
+		$('.header_icon__search').addClass('search_active')
+		$('.search_input').addClass('search_input_active')
+	}
+	function searchClose () {
+
+		$('.header_icon__search').removeClass('search_active')
+		$('.search_input').removeClass('search_input_active')
+	}
+
+	$('.search_click').on('click',function () {
+		searchOpen()
+	})
+	$(document).on('click',function (e) {
+		if (!e.target.classList.contains('search_click')) {
+			searchClose()
+		}
+	})
+	let active_slide = 0
+	
+	$('.main_slider_img').width($('.main_left').width())
+	$(window).on('resize',function () {
+		$('.main_slider_img').width($('.main_left').width())
+	})
+	$('.header_bur').on('click',function () {
+		$('.header_open').toggleClass('header_open_noactive')
+		console.log('123');
+	})
+	
+	
+
+})

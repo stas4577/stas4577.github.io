fc280591fd7fca1aa1b3ea065a5af7eea7bb7670
stas4577 <54317704+stas4577@users.noreply.github.com>
2020-07-13 00:22:54 +0300

    Step

---
+++ step/scripts/script.js
@@ -0,0 +1,18 @@
+'use strict'
+
+
+$(document).ready(function () {
+	
+	$('.burger__click').on('click',function () {
+		$('.burger__menu').toggleClass('burger__active')
+		$('.burger__click').toggleClass('burger__close')
+	})
+	$('.burger__link').on('click',function () {
+		let active_link = $(this).data('index')
+		$('.burger__link__more').removeClass('burger__link__more__active')
+		$('.burger__link__more').eq(active_link).addClass('burger__link__more__active')
+
+	})
+	
+
+})

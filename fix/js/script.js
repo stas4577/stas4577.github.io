247201819c152c25f09da27239b0d3181ce6899b
stas4577 <54317704+stas4577@users.noreply.github.com>
2020-08-26 20:54:48 +0300

    fix

---
+++ fix/js/script.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function () {
+	// Паралакс для телефона
+	$('html').on('mousemove',function (e) {
+		let mult = 15
+		let phone_top = (e.pageY / $(window).height() - 0.5).toFixed(100) * 2 
+		let phone_left = (e.pageX / $(window).width() - 0.5).toFixed(100) * 2
+		$('#phoneMove').css('top',phone_top * mult)
+		$('#phoneMove').css('left',phone_left * mult)
+	})
+
+	//	Бургер меню
+	function burgerSwitcher () {
+		$('.burger_menu').toggleClass('burger_active')
+		$('#grey_window').toggleClass('grey_active')
+		if ($('.burger_menu').hasClass('burger_active')) {
+			$('#burgerSwitcher').attr('src','img/burger_active.svg')
+		} else {
+			$('#burgerSwitcher').attr('src','img/burger.svg')
+		}
+	}
+	$('#burgerSwitcher').on('click',burgerSwitcher)
+})

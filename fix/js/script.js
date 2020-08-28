0bd0aec7029769db66b0e417d3aa226930e1803f
stas4577 <54317704+stas4577@users.noreply.github.com>
2020-08-28 21:37:51 +0300

    fix

--- fix/js/script.js
+++ fix/js/script.js
@@ -5,8 +5,7 @@ $(document).ready(function () {
 		let mult = 15
 		let phone_top = (e.pageY / $(window).height() - 0.5).toFixed(100) * 2 
 		let phone_left = (e.pageX / $(window).width() - 0.5).toFixed(100) * 2
-		$('#phoneMove').css('top',phone_top * mult)
-		$('#phoneMove').css('left',phone_left * mult)
+		$('#phoneMove').css('transform','translate(' + phone_left * mult + 'px,' + phone_top * mult + 'px)')
 	})
 
 	//	Бургер меню
@@ -20,4 +19,31 @@ $(document).ready(function () {
 		}
 	}
 	$('#burgerSwitcher').on('click',burgerSwitcher)
+
+	// Меню второго уровня
+	let active_second_menu = false
+	$('.burger__link').on('click',function () {
+		menuSecondOpen($(this))
+	})
+	function menuSecondOpen (el) {
+		let menu_selected = el.find('.burger__menu__second')
+		$('.burger__link').addClass('burger__link_noactive')
+		el.removeClass('burger__link_noactive')
+		el.attr('data-border','hide')
+		if (active_second_menu === menu_selected.data('menu')) {
+			menu_selected.removeClass('burger__menu__second_active')
+			active_second_menu = false
+			$('.burger__link').removeClass('burger__link_noactive')
+			el.attr('data-border','')
+		} else {
+			$('.burger__menu__second').removeClass('burger__menu__second_active')
+			menu_selected.addClass('burger__menu__second_active')
+			active_second_menu = menu_selected.data('menu')
+			el.attr('data-border','hide')
+		}
+	}
+	//Закрытие бургера при клике вне его
+	$('#grey_window').on('click',function (e) {
+		burgerSwitcher ()
+	})
 })

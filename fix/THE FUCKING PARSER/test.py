e146102f3df302e9f206d6aa4f6b5c4baf4a80a8
stas4577 <54317704+stas4577@users.noreply.github.com>
2020-09-26 10:36:54 +0300

    fix

--- fix/THE FUCKING PARSER/test.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome('C:\\Users\\Стас\\Desktop\\работа\\fix — копия\\THE FUCKING PARSER\\chromedriver.exe')
-driver.get("https://ifix-it.ru/remont-iphone")
-html = driver.page_source
-while html.find('data-elementor-open-lightbox') != -1:
-	pos = html.find('data-elementor-open-lightbox')
-	mask = html[]
--- fix/TheFuckingParser/setting.py
+++
@@ -1,3 +0,0 @@
-from bs4 import BeautifulSoup as BS
-import requests as req
-
